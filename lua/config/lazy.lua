9e77e1faa4b8f755b142d16c43cdc1ce33cc97d6
BMilliet <brunomilliet89@gmail.com>
2025-09-07 22:07:41 +0100

    chore(main): added completions

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,50 @@ require("lazy").setup({
         },
 
         {
+                "hrsh7th/nvim-cmp",
+                dependencies = {
+                        "hrsh7th/cmp-nvim-lsp",
+                        "hrsh7th/cmp-buffer",
+                        "hrsh7th/cmp-path",
+                        "hrsh7th/cmp-cmdline",
+                },
+                config = function()
+                        local cmp = require('cmp')
+                        vim.o.completeopt = "menu,menuone,noselect"
+                        cmp.setup({
+                                snippet = {
+                                        expand = function(args)
+                                                -- You can add luasnip or vsnip here if you use snippets
+                                        end,
+                                },
+                                mapping = {
+                                        ['<C-Space>'] = cmp.mapping.complete(),
+                                        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                                        ['<Down>'] = cmp.mapping.select_next_item(),
+                                        ['<Up>'] = cmp.mapping.select_prev_item(),
+                                },
+                                sources = cmp.config.sources({
+                                        { name = 'nvim_lsp' },
+                                        { name = 'buffer' },
+                                        { name = 'path' },
+                                })
+                        })
+                        -- Optional: cmdline completion
+                        cmp.setup.cmdline(':', {
+                                sources = {
+                                        { name = 'path' },
+                                        { name = 'cmdline' },
+                                }
+                        })
+                        cmp.setup.cmdline('/', {
+                                sources = {
+                                        { name = 'buffer' }
+                                }
+                        })
+                end,
+        },
+
+        {
                 -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
                 -- used for completion, annotations and signatures of Neovim apis
                 'folke/lazydev.nvim',
