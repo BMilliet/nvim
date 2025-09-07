bf3274018d02dcd54463bb45045c99beab326819
BMilliet <brunomilliet89@gmail.com>
2025-09-07 22:20:46 +0100

    refactor(main): move plugin files

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,99 +14,15 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-        { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+local plugins = {
+        require("config.lazy.telescope"),
+        require("config.lazy.mason-lspconfig"),
+        require("config.lazy.nvim-cmp"),
+        require("config.lazy.lazydev"),
+        require("config.lazy.fidget"),
+        require("config.lazy.oil"),
+        require("config.lazy.treesitter"),
+        require("config.lazy.rose-pine"),
+}
 
-        {
-                "mason-org/mason-lspconfig.nvim",
-                opts = {
-                        ensure_installed = { "lua_ls", "yamlls", "gopls", "jsonls" },
-                },
-                dependencies = {
-                        { "mason-org/mason.nvim", opts = {} },
-                        "neovim/nvim-lspconfig",
-                },
-        },
-
-        {
-                "hrsh7th/nvim-cmp",
-                dependencies = {
-                        "hrsh7th/cmp-nvim-lsp",
-                        "hrsh7th/cmp-buffer",
-                        "hrsh7th/cmp-path",
-                        "hrsh7th/cmp-cmdline",
-                },
-                config = function()
-                        local cmp = require('cmp')
-                        vim.o.completeopt = "menu,menuone,noselect"
-                        cmp.setup({
-                                snippet = {
-                                        expand = function(args)
-                                                -- You can add luasnip or vsnip here if you use snippets
-                                        end,
-                                },
-                                mapping = {
-                                        ['<C-Space>'] = cmp.mapping.complete(),
-                                        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                                        ['<Down>'] = cmp.mapping.select_next_item(),
-                                        ['<Up>'] = cmp.mapping.select_prev_item(),
-                                },
-                                sources = cmp.config.sources({
-                                        { name = 'nvim_lsp' },
-                                        { name = 'buffer' },
-                                        { name = 'path' },
-                                })
-                        })
-                        -- Optional: cmdline completion
-                        cmp.setup.cmdline(':', {
-                                sources = {
-                                        { name = 'path' },
-                                        { name = 'cmdline' },
-                                }
-                        })
-                        cmp.setup.cmdline('/', {
-                                sources = {
-                                        { name = 'buffer' }
-                                }
-                        })
-                end,
-        },
-
-        {
-                -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-                -- used for completion, annotations and signatures of Neovim apis
-                'folke/lazydev.nvim',
-                ft = 'lua',
-                opts = {
-                        library = {
-                                -- Load luvit types when the `vim.uv` word is found
-                                { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-                        },
-                },
-        },
-
-        { 'j-hui/fidget.nvim',             opts = {} },
-
-        {
-                'stevearc/oil.nvim',
-                ---@module 'oil'
-                ---@type oil.SetupOpts
-                opts = {},
-                -- Optional dependencies
-                -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-                -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-                -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-                lazy = false,
-        },
-
-        { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" },
-
-
-        {
-                "rose-pine/neovim",
-                name = "rose-pine",
-                config = function()
-                        vim.cmd("colorscheme rose-pine")
-                end
-        },
-})
+require("lazy").setup(plugins)
