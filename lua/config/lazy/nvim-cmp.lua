6d676d7b62b7a87e5ca742991d9e44e62dcd2b75
BMilliet <brunomilliet89@gmail.com>
2026-01-11 11:35:15 +0000

    chore(main): add luaSnips

--- lua/config/lazy/nvim-cmp.lua
+++ lua/config/lazy/nvim-cmp.lua
@@ -5,36 +5,57 @@ return {
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
+        "L3MON4D3/LuaSnip", -- necessário se usar snippets
+        "saadparwaiz1/cmp_luasnip",
     },
     config = function()
         local cmp = require('cmp')
-        vim.o.completeopt = "menu,menuone,noselect"
+        local luasnip = require('luasnip')
+
+        vim.o.completeopt = "menu,menuone,noinsert" -- noinsert evita inserir texto automaticamente
+
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    -- You can add luasnip or vsnip here if you use snippets
+                    luasnip.lsp_expand(args.body)
                 end,
             },
             mapping = {
                 ['<C-Space>'] = cmp.mapping.complete(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 ['<Down>'] = cmp.mapping.select_next_item(),
                 ['<Up>'] = cmp.mapping.select_prev_item(),
+                ['<Right>'] = cmp.mapping.close(),
+                ['<CR>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+                    else
+                        fallback() -- se o menu não estiver aberto, insere um Enter normal
+                    end
+                end, { "i", "s" }),
             },
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'buffer' },
                 { name = 'path' },
-            })
+                { name = 'luasnip' },
+            }),
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
         })
-        -- Optional: cmdline completion
+
+        -- Cmdline
         cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
             sources = {
                 { name = 'path' },
                 { name = 'cmdline' },
             }
         })
+
         cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
             sources = {
                 { name = 'buffer' }
             }
