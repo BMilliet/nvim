5565afc1ac76b6d30a2a341aaef1726304cc825d
BMilliet <brunomilliet89@gmail.com>
2026-04-23 12:01:56 +0100

    chore(main): improved highlight selection

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -4,6 +4,8 @@ function ColorMyPencils(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Visual", { bg = "#f9e2af", fg = "#1e1e2e" })
+    vim.api.nvim_set_hl(0, "VisualNOS", { bg = "#f9e2af", fg = "#1e1e2e" })
 end
 
 ColorMyPencils()
