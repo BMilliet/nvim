3dbeb7330745e161f87f9b448ae1d4ca7033780e
BMilliet <brunomilliet89@gmail.com>
2026-04-28 11:56:40 +0100

    chore(main): remove conflict

--- lua/config/help.lua
+++ lua/config/help.lua
@@ -73,7 +73,6 @@ vim.api.nvim_create_user_command("Help", function()
             items = {
                 { ":", "Blame", "Open blame for current file" },
                 { "n", "<leader>t", "Open changed file list" },
-                { ":", "Conflict", "Open git conflict resolver" },
                 { ":", "Status", "Open changed file list" },
                 { ":", "GitStatusRefresh", "Refresh git signs and scrollbar" },
                 { ":", "GitStatusToggle", "Toggle git signs and scrollbar" },
--- lua/config/lazy/git-status.lua
+++ lua/config/lazy/git-status.lua
@@ -7,7 +7,6 @@ return {
     },
     cmd = {
       "Blame",
-      "Conflict",
       "Status",
       "GitStatusRefresh",
       "GitStatusToggle",
